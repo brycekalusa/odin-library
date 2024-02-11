2eb3f6dfeb497faf91363327a8ea3e2058a806f4
Bryce Kalusa <brycekalusa.dev@gmail.com>
2024-02-10 21:05:50 -0700

    Added constructor to class function

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const addBtn = document.querySelector('#add-btn');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book{
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 Book.prototype.changeRead = function() {
